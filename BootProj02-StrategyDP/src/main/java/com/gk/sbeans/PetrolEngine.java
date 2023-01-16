f09b1781abf5ff0787289ae58797950c3da0f9da
gaurkroab <gauravsharma815836@gmail.com>
2023-01-16 19:11:18 +0530

    Strategy Design pattern

---
+++ BootProj02-StrategyDP/src/main/java/com/gk/sbeans/PetrolEngine.java
@@ -0,0 +1,23 @@
+package com.gk.sbeans;
+
+import org.springframework.stereotype.Component;
+
+@Component("pEngg")
+public class PetrolEngine implements Engine {
+	
+	public PetrolEngine() {
+		System.out.println("PetrolEngine:: 0- param constructor");
+	}
+	@Override
+	public void start() {
+		System.out.println("PetrolEngine.start()");
+
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("PetrolEngine.stop()");
+
+	}
+
+}
